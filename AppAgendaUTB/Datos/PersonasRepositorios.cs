0e02c48dc0a21d5e66ec3afa85e70934369f4e19
alfonsopate <alfonscor@gmail.com>
2015-03-03 14:54:20 -0500

    luis 03-03

---
+++ AppAgendaUTB/Datos/PersonasRepositorios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppAgendaUTB.Modelo;
+
+
+
+namespace AppAgendaUTB.Datos
+{
+    public class PersonasRepositorios
+    {
+
+        private List<Personas> data =new List<Personas>()
+
+        public IEnumerable<Personas>getPersonas()
+        {
+            Personas p1 = new Personas();
+            Personas p2 = new Personas();
+        }
+    }
+}
